38761e213e947258bc33940a911df7f7bf6a7d69
Ionésio Junior <ionesiojr@gmail.com>
2024-11-13 19:17:51 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ if [ ! -d ".venv" ]; then
     echo "Virtual environment not found. Creating one..."
     uv venv .venv
     echo "Virtual environment created successfully."
-    uv pip install -U syftbox
+    uv pip install -U syftbox --quiet
     # uv pip install -r requirements.txt
 else
     echo "Virtual environment already exists."
