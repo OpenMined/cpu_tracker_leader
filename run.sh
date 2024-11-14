5ab7077eff243b14c59d2e94c00a3377fd11d83e
Ionésio Junior <ionesiojr@gmail.com>
2024-11-14 18:03:03 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,17 +5,18 @@ if [ ! -d ".venv" ]; then
     echo "Virtual environment not found. Creating one..."
     uv venv -p 3.12 .venv
     echo "Virtual environment created successfully."
-    uv pip install -U syftbox --quiet
+
     # uv pip install -r requirements.txt
 else
     echo "Virtual environment already exists."
 fi
 
+uv pip install -U syftbox --quiet
 . .venv/bin/activate
 
 # # run app using python from venv
 echo "Running cpu_tracker_leader with $(python3 --version) at '$(which python3)'"
-uv run python3 main.py
+python3 main.py
 
 # # deactivate the virtual environment
 deactivate
